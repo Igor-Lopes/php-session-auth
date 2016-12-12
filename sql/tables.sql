41412028577eca0bc90931197be9e2d0a03d7294
Igor Lopes <igor@nucleus.eti.br>
2016-12-12 20:37:16 -0300

    Added IF NOT EXISTS to tables creation

--- sql/tables.sql
+++ sql/tables.sql
@@ -3,21 +3,21 @@
 -- Copyright (c) Nucleus Inc (https://www.nucleus.eti.br)
 -- Licensed under the MIT License (https://opensource.org/licenses/MIT)
 
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
      `user_id` INT AUTO_INCREMENT PRIMARY KEY,
      `username` VARCHAR(255) NOT NULL,
      `email` VARCHAR(255) NOT NULL UNIQUE,
      `is_active` ENUM ('Y','N') DEFAULT 'N',
      `password` CHAR(128) NOT NULL
 );
-CREATE TABLE attempts (
+CREATE TABLE IF NOT EXISTS attempts (
     `attempt_id` INT AUTO_INCREMENT PRIMARY KEY,
     `user_id` INT NOT NULL,
     `ip` VARCHAR(39) NOT NULL,
     `expire_time` VARCHAR(30) NOT NULL,
     FOREIGN KEY(`user_id`) REFERENCES users(`user_id`) ON DELETE CASCADE
 );
-CREATE TABLE sessions (
+CREATE TABLE IF NOT EXISTS sessions (
   `session_id` INT AUTO_INCREMENT PRIMARY KEY,
   `user_id` INT NOT NULL,
   `hash` VARCHAR(255) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE sessions (
   `ip` VARCHAR(39) NOT NULL,
   FOREIGN KEY(`user_id`) REFERENCES users(`user_id`) ON DELETE CASCADE
 );
-CREATE TABLE requests (
+CREATE TABLE IF NOT EXISTS requests (
   `request_id` INT AUTO_INCREMENT PRIMARY KEY,
   `user_id` INT NOT NULL,
   `request_key` VARCHAR(20) NOT NULL,
