7863530c24d03b203b95b12393d6340dec1c9ad6
Igor Lopes <igor@nucleus.eti.br>
2016-12-12 22:22:12 -0300

    Testing db connection and insert

--- tests/Test.php
+++ tests/Test.php
@@ -1,7 +1,22 @@
 <?php
+
 class Test extends PHPUnit_Framework_TestCase{
-	public function testTravis() {
-		$this->assertEquals(1+1,2);
-  	}
+
+    private $conn;
+
+    public function __construct(){
+        $this->conn = new mysqli('127.0.0.1', 'root', '', 'simple_auth_test');
+    }
+
+    public function testDbCon(){
+        $Username = 'Admin';
+        $password = 'admin';
+        $email = 'admin@email.com';
+
+        $password = hash('sha512', $password);
+        $password = password_hash($password, PASSWORD_BCRYPT);
+        $stmt = $this->conn->prepare('INSERT INTO users (Username, email, password) VALUES (?, ?, ?)');
+        $stmt->bind_param('sss', $Username, $email, $password);
+        $this->assertTrue($stmt->execute());
+    }
 }
-?>
