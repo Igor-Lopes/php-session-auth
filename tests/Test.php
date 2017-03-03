48538bb4805c583a736e1f78fc734db77125a232
Igor Lopes <igor@nucleus.eti.br>
2017-03-03 11:37:27 -0300

    Replace mysqli for pdo

--- tests/Test.php
+++ tests/Test.php
@@ -1,8 +1,21 @@
 <?php
 class Test extends PHPUnit_Framework_TestCase{
+
+    private $params;
     private $conn;
+
     public function __construct(){
-        $this->conn = new mysqli('127.0.0.1', 'root', '', 'simple_auth_test');
+
+      $this->params = array(
+        'db_host' => '127.0.0.1',
+        'db_user' => 'root',
+        'db_password' => '',
+        'db_name' => 'simple_auth_test',
+        );
+
+      $connString = 'mysql:host='.$this->params['db_host'].';dbname='.$this->params['db_name'];
+      
+      $this->conn = new PDO($connString, $this->params['db_user'], $this->params['db_password']);
     }
     public function testDbCon(){
         $Username = 'Admin';
@@ -10,8 +23,10 @@ class Test extends PHPUnit_Framework_TestCase{
         $email = 'admin@email.com';
         $password = hash('sha512', $password);
         $password = password_hash($password, PASSWORD_BCRYPT);
-        $stmt = $this->conn->prepare('INSERT INTO users (Username, email, password) VALUES (?, ?, ?)');
-        $stmt->bind_param('sss', $Username, $email, $password);
+        $stmt = $this->conn->prepare('INSERT INTO `users` (Username, email, password) VALUES (:Username, :email, :password)');
+        $stmt->bindParam(':Username', $Username, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
         $this->assertTrue($stmt->execute());
     }
 }
