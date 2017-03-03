bbdffa13787f47e84ccaaf882a2d1067fbd5aea3
Igor Lopes <igor@nucleus.eti.br>
2017-03-03 13:14:32 -0300

    Update test

--- tests/Test.php
+++ tests/Test.php
@@ -1,24 +1,60 @@
 <?php
 
-require_once __DIR__ . '/../src/SimpleAuth.php';
+require_once __DIR__.'/../src/SimpleAuth.php';
 
-class Test extends PHPUnit_Framework_TestCase{
-
+class Test extends PHPUnit_Framework_TestCase
+{
     private $simpleAuth;
 
-    public function __construct(){
-
-      $params = array(
+    public function __construct()
+    {
+        $params = array(
         'db_host' => '127.0.0.1',
         'db_user' => 'root',
         'db_password' => '',
         'db_name' => 'simple_auth_test',
         );
 
-      $this->simpleAuth = new SimpleAuth($params);
-
+        $this->simpleAuth = new SimpleAuth($params);
     }
-    public function testDbCon(){
+
+    public function testCreateUser()
+    {
+        //Successful:
         $this->assertTrue($this->simpleAuth->createUser('Igor', 'test@email.com', 'test')['success']);
+        //Failed - Same email:
+        $this->assertFalse($this->simpleAuth->createUser('Igor', 'test@email.com', 'test')['success']);
+    }
+
+    public function testLogin()
+    {
+        //Failed: Incorrect Password
+        $this->assertFalse($this->simpleAuth->login('test@email.com', 'incorrect')['success']);
+        //Failed: Incorrect Email
+        $this->assertFalse($this->simpleAuth->login('incorrect@email.com', 'test')['success']);
+        //Successful:
+        $this->assertTrue($this->simpleAuth->login('test@email.com', 'test')['success']);
+    }
+
+    public function testUpdatePassword()
+    {
+        //Failed: Incorrect Email
+        $this->assertFalse($this->simpleAuth->login('incorrect@email.com', 'test', 'new_password')['success']);
+        //Failed: Incorrect current Password
+        $this->assertFalse($this->simpleAuth->updatePassword('test@email.com', 'incorrect', 'new_password')['success']);
+        //Successful:
+        $this->assertTrue($this->simpleAuth->updatePassword('test@email.com', 'test', 'new_password')['success']);
+        //Test Login with new password:
+        $this->assertTrue($this->simpleAuth->login('test@email.com', 'new_password')['success']);
+    }
+
+    public function testBruteForce()
+    {
+        //Lock account:
+        for ($i = 0; $i < 6; ++$i) {
+            $this->simpleAuth->login('test@email.com', 'incorrect');
+        }
+        //Correct credentials:
+        $this->assertFalse($this->simpleAuth->login('test@email.com', 'test')['success']);
     }
 }
